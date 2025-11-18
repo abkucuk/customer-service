2fd634f1b508d7e29ff0cedc3020930522559208
ahmetbkucuk <ahmetbkucuk@gmail.com>
2025-11-18 16:49:54 +0300

    Refactor CustomerController to return ResponseEntity and add new endpoints for customer limit checks and available credit

--- src/main/java/com/bankcase/customer_service/infra/controller/CustomerController.java
+++ src/main/java/com/bankcase/customer_service/infra/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.bankcase.customer_service.application.dto.CustomerRequest;
 import com.bankcase.customer_service.application.dto.CustomerResponse;
 import com.bankcase.customer_service.application.service.CustomerApplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -16,12 +17,22 @@ public class CustomerController {
     private final CustomerApplicationService service;
 
     @PostMapping
-    public CustomerResponse create(@RequestBody CustomerRequest request) {
-        return service.create(request);
+    public ResponseEntity<CustomerResponse> create(@RequestBody CustomerRequest request) {
+        return ResponseEntity.ok(service.create(request));
     }
 
-    @GetMapping("/{id}")
-    public CustomerResponse getById(@PathVariable Long id) {
-        return service.get(id);
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerResponse> getById(@PathVariable Long customerId) {
+        return ResponseEntity.ok(service.get(customerId));
+    }
+
+    @GetMapping("/{customerId}/has-enough-limit")
+    public ResponseEntity<Boolean> hasEnoughLimit(@PathVariable Long customerId, @RequestParam BigDecimal amount){
+        return ResponseEntity.ok(service.hasEnoughLimit(customerId, amount));
+    }
+
+    @GetMapping("/{customerId}/available-credit")
+    public ResponseEntity<BigDecimal> check(@PathVariable Long customerId) {
+        return ResponseEntity.ok(service.availableCredit(customerId));
     }
 }
